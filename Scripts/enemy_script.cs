193ae9bae9779493776922e315fe87418bb015db
Sseptimus <69606116+Sseptimus@users.noreply.github.com>
2023-05-19 13:30:36 +1200

    Added Health Bar (Among other things)
    Added health bar. Changed character movement to physics based and in the process of converting dash and block to physics

--- Scripts/enemy_script.cs
+++ Scripts/enemy_script.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class enemy_script : Area2D
 {
-	Area2D player;
+	CharacterBody2D player;
 	AnimationPlayer enemy_melee;
 	TextureProgressBar healthbar;
 	TextureProgressBar health;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		player = GetTree().Root.GetNode<Area2D>("Movement_Test/Player");
+		player = GetTree().Root.GetNode<CharacterBody2D>("Movement_Test/Player");
 		enemy_melee = GetNode<AnimationPlayer>("Sprite2D/AnimationPlayer");
 		healthbar = player.GetNode<TextureProgressBar>("Health_Bar_Container/Health_Bar");
 		health = GetNode<TextureProgressBar>("Health_Bar_Container/Health_Bar");
@@ -22,13 +22,20 @@ public partial class enemy_script : Area2D
 		Vector2 current = Vector2.Zero;
 		LookAt(player.Position);
 		health.GetParent<Node2D>().GlobalRotationDegrees = 0;
-		MoveLocalX(200*(float)delta);
-		if(Position.DistanceTo(player.Position)<=300){
+		
+		if(Position.DistanceTo(player.Position)<=130){
 			enemy_melee.Play("Enemy_melee");
 		}
+		if(Position.DistanceTo(player.Position)<=75){
+			MoveLocalX(-400*(float)delta);
+		}else{
+			MoveLocalX(200*(float)delta);
+		}
+			
+
 	}
 	public void _on_hitbox_container_area_entered(Area2D area){
-		if(area.Name == "Player"){
+		if(area.Name == "Player_hitbox_container"){
 			healthbar.Value -= 50;
 		}
 	}
--- Scripts/player_script.cs
+++ Scripts/player_script.cs
@@ -1,54 +1,143 @@
 using Godot;
 using System;
 
-public partial class player_script : Area2D
+public partial class player_script : CharacterBody2D
 {
-	float speed = 400;
-	AnimationPlayer attack_anim;
+	public const float Speed = 300.0f;
+	
+	AnimationPlayer anim;
 	String facing;
 	Sprite2D sprite;
 	TextureProgressBar health;
+	Vector2 ScreenSize;
+	bool attack_waiting = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		attack_anim = GetNode<AnimationPlayer>("Sprite2D/AnimationPlayer");
+		anim = GetNode<AnimationPlayer>("Sprite2D/AnimationPlayer");
 		health = GetNode<TextureProgressBar>("Health_Bar_Container/Health_Bar");
-		
+		ScreenSize = GetViewportRect().Size;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Vector2.Zero;
-		if(Input.IsActionPressed("ui_up")){
-			velocity.Y -= 1;
-			RotationDegrees = -90;
-		}if(Input.IsActionPressed("ui_down")){
-			velocity.Y += 1;
+		
+		Vector2 velocity = Velocity;
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(velocity.Y,0,Speed);
+		}
+		switch(direction.Y){
+			case 1:
 			RotationDegrees = 90;
-		}if(Input.IsActionPressed("ui_left")){
-			velocity.X -= 1;
-			RotationDegrees = 180;
-		}if(Input.IsActionPressed("ui_right")){
-			velocity.X += 1;
+			break;
+			case -1:
+			RotationDegrees = -90;
+			break;
+		}switch(direction.X){
+			case 1:
 			RotationDegrees = 0;
+			break;
+			case -1:
+			RotationDegrees = 180;
+			break;
 		}
-		velocity = velocity.Normalized() * speed;
-		velocity.X *= (float)delta;
-		velocity.Y *= (float)delta;
-		Position += velocity;
+		Velocity = velocity;
+		MoveAndSlide();
 		if(Input.IsActionPressed("Attack")){
-			attack_anim.Play("Player_melee");
+			if(!attack_waiting && anim.CurrentAnimation == ""){
+				anim.Play("Player_melee");
+				attack_delay();
+			}
+			
 		}
+		if(Input.IsActionPressed("Block")){
+			if(anim.CurrentAnimation == ""){
+				anim.Play("Player_block");
+			}
+		}
+		if(Input.IsActionPressed("Dash")){
+			if(anim.CurrentAnimation == ""){
+				anim.Play("Player_dash");
+				
+			}
+		}
+			
+		
 		if(health.Value<=0){
 			QueueFree();
 			GetTree().Quit();
 		}
 		health.GetParent<Node2D>().GlobalRotationDegrees = 0;
 	}
+	async void attack_delay(){
+		attack_waiting = true;
+		await ToSignal(GetTree().CreateTimer(3), "timeout");
+		attack_waiting = false;	
+	}	
 	public void _on_attack_hitbox_container_area_entered(Area2D area){
 		if(area.Name == "Enemy"){
 			GetTree().Root.GetNode<TextureProgressBar>($"Movement_Test/{area.Name}/Health_Bar_Container/Health_Bar").Value -= 10;
 		}
 	}
 }
+/*
+using Godot;
+using System;
+
+public partial class player_script : CharacterBody2D
+{
+	float speed = 400;
+	AnimationPlayer anim;
+	String facing;
+	Sprite2D sprite;
+	TextureProgressBar health;
+	Vector2 ScreenSize;
+	bool attack_waiting = false;
+	
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		
+		Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Velocity = inputDirection * speed;
+		MoveAndSlide();
+		if(Input.IsActionPressed("Attack")){
+			if(!attack_waiting && anim.CurrentAnimation == ""){
+				anim.Play("Player_melee");
+				attack_delay();
+			}
+			
+		}
+		if(Input.IsActionPressed("Block")){
+			if(anim.CurrentAnimation == ""){
+				anim.Play("Player_block");
+			}
+		}
+		if(Input.IsActionPressed("Dash")){
+			if(anim.CurrentAnimation == ""){
+				anim.Play("Player_dash");
+				
+			}
+		}
+			
+		
+		if(health.Value<=0){
+			QueueFree();
+			GetTree().Quit();
+		}
+		health.GetParent<Node2D>().GlobalRotationDegrees = 0;
+	}
+	
+	
+}
+*/
